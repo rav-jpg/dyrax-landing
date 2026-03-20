fa5075a7f6261cd5813b85ec788875141f944b04
rav-jpg <kr.ravi5565@gmail.com>
2026-03-20 23:35:07 +0530

    Create layout.jsx

---
+++ app/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Dyrax",
+  description: "Dyrax Meta Ads landing page",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
